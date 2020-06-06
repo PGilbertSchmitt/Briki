e2fa0dd96f3c7de3f976ff9b38e07a3284c47b5c
PGilbertSchmitt <pgilbertschmitt@gmail.com>
2020-06-06 09:35:39 -0700

    Initial commit

---
+++ src/react/tiki.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Spectrum is GREEN');
+  ReactDOM.render(
+    <h2>Ey, how ya doin?</h2>,
+    document.getElementById('root'),
+  );
+});
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const tsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+var babelOptions = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/preset-react"
+  ],
+  plugins: [
+    "@babel/proposal-class-properties"
+  ]
+};
+
+module.exports = {
+  context: __dirname,
+  target: "electron-renderer",
+  mode: "development",
+  entry: {
+    main: "./src/main.ts",
+    renderer: "./src/react/tiki.tsx"
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: "[name].js"
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"),
+      "node_modules"
+    ],
+    extensions: [".js", ".ts", ".tsx", "scss"],
+    plugins: [
+      new tsConfigPathsPlugin()
+    ]
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "source-map-loader"
+      }
+    ]
+  },
+  node: {
+    fs: "empty",
+    __dirname: false,
+  }
+};
